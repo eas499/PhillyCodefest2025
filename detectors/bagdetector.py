7220e99f9680895dda7d07026d2eb4b640fe6bee
PrecognizantWaffles <67386467+PrecognizantWaffles@users.noreply.github.com>
2025-03-01 16:58:28 -0500

    like a plastic bag

--- detectors/bagdetector.py
+++ detectors/bagdetector.py
@@ -9,7 +9,7 @@ class BagDetector:
 		self.accumWeight = accumWeight
 		# initialize the background model
 		self.bg = None   
-		self.yolo = YOLO('best.pt')
+		self.yolo = YOLO('katy_perry.pt')
 
 	def update(self, image):
 		# if the background model is None, initialize it
